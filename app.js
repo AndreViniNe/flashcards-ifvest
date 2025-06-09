429980a04ebd3f248888b9e67c7b3aca60aae957
Andre Vinicius <andrevini.neves@gmail.com>
2025-06-09 19:25:48 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('flashcards');
+});
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
